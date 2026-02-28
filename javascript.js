c89f6b6c5201a13acf5f5479fe2354a4537a4284
Vboxuser <127411353+StepByStep2002@users.noreply.github.com>
2026-02-28 18:28:21 +0200

    Added a button to generate the new grid
    This button deletes the old grid, asks for the size of the
    new grid and then generates it, if no value is entered or
    the value is bigger than 100, the grid won't be generated
    and alert message is shown instead.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const mainDiv = document.getElementById("mainDiv");
+const changeGrid = document.getElementById("changeGrid")
 for(let i = 0; i < 16; i++)
 {
     let createDivVertical = document.createElement("div");
@@ -15,3 +16,36 @@ for(let i = 0; i < 16; i++)
         createDivVertical.appendChild(createDivHorizontal);
     }
 }
+
+changeGrid.addEventListener("click", (e) =>{
+    let removeGrid = document.getElementsByClassName("gridDivVertical");
+    while(removeGrid.length > 0)
+    {
+        removeGrid[0].parentNode.removeChild(removeGrid[0]); 
+    }
+
+    let newGrid = prompt("Enter the number of new grids to generate horizontally and vertically")
+    if (newGrid == "" || newGrid > 100)
+    {
+        return alert("Wrong value, can't generate a new grid")
+    }
+    else
+    
+
+    for(let i = 0; i < newGrid; i++)
+    {
+    let createDivVertical = document.createElement("div");
+    createDivVertical.className = "gridDivVertical"
+    mainDiv.appendChild(createDivVertical);
+
+        for(let j = 0; j < newGrid; j++)
+        {
+        let createDivHorizontal = document.createElement("div");
+        createDivHorizontal.className = "gridDivHorizontal"
+        createDivHorizontal.addEventListener("mouseenter", (e) =>{
+            createDivHorizontal.className = "gridDivHorizontalColored"
+        })
+        createDivVertical.appendChild(createDivHorizontal);
+        }
+}
+});
